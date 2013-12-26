bcc5a0c055b5b8e66e65e924cad04692a2d5984f
root <root@creepypastas.cloud>
2013-12-26 07:14:05 +0000

    Removing default featured image, since it looks ugly on creepypastas.com (too many posts with no featured image, or images at all)

--- content.php
+++ content.php
@@ -5,8 +5,6 @@
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 				<?php if ( has_post_thumbnail() ): ?>
 					<?php the_post_thumbnail('thumb-medium'); ?>
-				<?php else: ?>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/thumb-medium.png" alt="<?php the_title(); ?>" />
 				<?php endif; ?>
 				<?php if ( has_post_format('video') && !is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-play"></i></span>'; ?>
 				<?php if ( has_post_format('audio') && !is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-volume-up"></i></span>'; ?>
